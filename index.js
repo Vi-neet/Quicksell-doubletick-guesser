d6c7c50308610bfbda76764db80afdb646a6cd4f
Vin <bhasinvineet49@gmail.com>
2023-11-06 11:25:31 +0530

    bug fixes

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ let resetButton;
 
 function checkGuess() {
   const userGuess = Number(guessField.value);
+  if (userGuess < 1 || userGuess > 100) {
+    alert('Please enter a number between 1 and 100.');
+    return; 
+  }
+
   if (guessCount === 1) {
     guesses.textContent = 'Previous guesses: ';
   }
